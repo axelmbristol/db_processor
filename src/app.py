554c47640a800630eef9eb90a5240ec313be7652
fo18103 <fo18103@bristol.ac.uk>
2018-11-07 19:31:55 +0000

    trying to implement cassandra.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 from tables import *
+from cassandra.cluster import Cluster
+
 import os
 import re
 
@@ -14,54 +16,73 @@ class Animal(IsDescription):
     second_sensor_value = StringCol(16)
 
 
-print('init mongoDB...')
-client = MongoClient('localhost', 27017)
-db_names = client.list_database_names()
+# print('init mongoDB...')
+# client = MongoClient('localhost', 27017)
+# db_names = client.list_database_names()
 
-file_name = "data.h5"
-print("purge...")
-os.remove(file_name)
-h5file = open_file(file_name, mode="a", title="database file")
+# file_name = "data.h5"
+# print("purge...")
+# os.remove(file_name)
+# h5file = open_file(file_name, mode="a", title="database file")
+#
+# for farm_id in db_names:
+#     if len(farm_id) != 11:
+#         continue
+#     print("farm id :"+farm_id)
+#     db = client[farm_id]
+#     colNames = db.list_collection_names()
+#     cpt = 0
+#     colNames.sort()
+#     group = h5file.create_group("/", "_"+farm_id, 'Farm id')
+#     for colName in colNames:
+#         print(str(cpt) + "/" + str(len(colNames)) + " " + colName + "...")
+#         collection = db[colName]
+#         animals = collection.find_one()["animals"]
+#         for animal in animals:
+#             tag_data = animal["tag_data"]
+#             serial_number = tag_data[0]["serial_number"]
+#
+#             node = "_"+str(serial_number)
+#             where = "/_"+farm_id
+#             if "_"+str(serial_number) not in group:
+#                 table = h5file.create_table(group, node, Animal, "Animal id")
+#             else:
+#                 table = h5file.get_node(where, node)
+#
+#             an = table.row
+#             for entry in tag_data:
+#                 an['time'] = str(entry["time"])
+#                 an['date'] = str(entry["date"])
+#                 an['serial_number'] = int(serial_number)
+#                 if entry['signal_strength'] is not None and re.sub("[^0-9]", "", entry["signal_strength"]) != '':
+#                     an['signal_strength'] = int(re.sub("[^0-9]", "", entry["signal_strength"]))
+#                 if entry['battery_voltage'] is not None and re.sub("[^0-9]", "", entry["battery_voltage"]) != '':
+#                     an['battery_voltage'] = int(re.sub("[^0-9]", "", entry["battery_voltage"]))
+#                 an['first_sensor_value'] = int(entry["first_sensor_value"])
+#                 if 'second_sensor_value' in entry:
+#                     an['second_sensor_value'] = str(entry["second_sensor_value"])
+#                 an.append()
+#             table.flush()
+#         cpt = cpt + 1
+#         # if cpt >= 1:
+#         #     break
+# print('finished')
 
-for farm_id in db_names:
-    if len(farm_id) != 11:
-        continue
-    print("farm id :"+farm_id)
-    db = client[farm_id]
-    colNames = db.list_collection_names()
-    cpt = 0
-    colNames.sort()
-    group = h5file.create_group("/", "_"+farm_id, 'Farm id')
-    for colName in colNames:
-        print(str(cpt) + "/" + str(len(colNames)) + " " + colName + "...")
-        collection = db[colName]
-        animals = collection.find_one()["animals"]
-        for animal in animals:
-            tag_data = animal["tag_data"]
-            serial_number = tag_data[0]["serial_number"]
 
-            node = "_"+str(serial_number)
-            where = "/_"+farm_id
-            if "_"+str(serial_number) not in group:
-                table = h5file.create_table(group, node, Animal, "Animal id")
-            else:
-                table = h5file.get_node(where, node)
+cluster = Cluster(['127.0.0.1'])
+session = cluster.connect()
 
-            an = table.row
-            for entry in tag_data:
-                an['time'] = str(entry["time"])
-                an['date'] = str(entry["date"])
-                an['serial_number'] = int(serial_number)
-                if entry['signal_strength'] is not None and re.sub("[^0-9]", "", entry["signal_strength"]) != '':
-                    an['signal_strength'] = int(re.sub("[^0-9]", "", entry["signal_strength"]))
-                if entry['battery_voltage'] is not None and re.sub("[^0-9]", "", entry["battery_voltage"]) != '':
-                    an['battery_voltage'] = int(re.sub("[^0-9]", "", entry["battery_voltage"]))
-                an['first_sensor_value'] = int(entry["first_sensor_value"])
-                if 'second_sensor_value' in entry:
-                    an['second_sensor_value'] = str(entry["second_sensor_value"])
-                an.append()
-            table.flush()
-        cpt = cpt + 1
-        # if cpt >= 1:
-        #     break
-print('finished')
+farmId = "farmid"
+animalId = "animalId"
+session.execute("""
+    CREATE KEYSPACE IF NOT EXISTS %s
+    WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+    """ % farmId)
+
+print("setting keyspace...")
+session.set_keyspace(farmId)
+
+print("creating table...")
+session.execute("CREATE TABLE IF NOT EXIST users(id int PRIMARY KEY, location Text)")
+
+#session.execute("CREATE TABLE IF NOT EXIST test(date Text,time Text,control_station Text,serial_number Text,signal_strength Int,battery_voltage Int,first_sensor_value Int,second_sensor_value Text,PRIMARY KEY( time, date ))")
